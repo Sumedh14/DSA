2f26024cb6d4a4475420a950343c4a9ccadc50bd
Sumedh14 <112951142+Sumedh14@users.noreply.github.com>
2024-08-25 13:11:06 +0530

    Groupanagram

--- Array/groupAnagram.js
+++ Array/groupAnagram.js
@@ -5,61 +5,25 @@ Group Anagram
 
 let arr = ["cat", "dog", "tacc", "god", "act"];
 
-// function groupingAnagram (arr) {
-//     const hashMap = new Map();
-//     groupWord(arr, hashMap);
-
-//     return [...hashMap.values()];
-// }
-
-// function groupWord (arr, hashMap) {
-//     for (const original of arr) {
-//         const hash = getWord(original);
-//         const values = hashMap.get(hash) || [];
-
-//         values.push(original);
-//         hashMap.set(hash, values);
-//     }
-// }
-
-// function getWord (word) {
-//     const frequency = new Array(26).fill(0);
-//     for (const char of word) {
-//         const code = charCode(char);
-//         frequency[code] += 1;
-//     }
-//     return buildHash(frequency);
-// }
-
-// const charCode = (char) => char.charCodeAt(0) - 'a'.charCodeAt(0);
-
-// const buildHash = (frequency) => frequency.toString();
-
-
 function groupingAnagram (arr) {
     let hashMap = new Map();
-    groupWords(arr, hashMap);
-    return [...hashMap.values()];
-}
-
-function groupWords (arr, hashMap) {
     for (let word of arr) {
         let hash = getHash(word);
-        const value = hashMap.get(hash) || [];
+        let value = hashMap.get(hash) || [];
 
         value.push(word);
         hashMap.set(hash, value);
     }
+    return [...hashMap.values()];
 }
 
 function getHash (word) {
-    let frequency = new Array(26).fill(0);
-    for (char of word) {
+    let freq = new Array(26).fill(0);
+    for (let char of word) {
         let charCode = getCode(char);
-        frequency[charCode] += 1;
+        freq[charCode] += 1;
     }
-
-    return decodeFre(frequency);
+    return decodeFre(freq);
 }
 
 function getCode (char) {
