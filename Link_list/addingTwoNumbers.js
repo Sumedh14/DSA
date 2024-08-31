567b32970c33d60ee480160995715b4c8a5a81f3
Sumedh14 <112951142+Sumedh14@users.noreply.github.com>
2024-08-31 12:46:54 +0530

    adding two numbers

---
+++ Link_list/addingTwoNumbers.js
@@ -0,0 +1,31 @@
+/*
+Add Two Numbers
+You are given two non-empty linked lists, l1 and l2, where each represents a non-negative integer.
+The digits are stored in reverse order, e.g. the number 123 is represented as 3 -> 2 -> 1 -> in the linked list.
+Each of the nodes contains a single digit. You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Input: l1 = [1,2,3], l2 = [4,5,6]
+
+Output: [5,7,9]
+
+Explanation: 321 + 654 = 975.
+*/
+
+const addNumber = (list1, list2) => {
+    let newlist = new LinkedList();
+    let current = newlist;
+
+    let carry = 0
+    while (list1 || list2 || carry) {
+        let l1 = list1 ? list1.value : 0;
+        let l2 = list2 ? list2.value : 0;
+
+        let value = l1 + l2 + carry;
+        carry = Math.floor(value / 10);
+        value = value % 10;
+
+        current.next = new LinkedList(value);
+        list1 = list1 ? list1.next : null;
+        list2 = list2 ? list2.next : null;
+    }
+    return newlist.next;
+}
