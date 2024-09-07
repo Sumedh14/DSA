68ab897fccd3c2e230be0860db5c5cdb0c63a1c2
Sumedh14 <112951142+Sumedh14@users.noreply.github.com>
2024-09-07 22:56:07 +0530

    Insert in position

---
+++ SerachingAlgo/serachInsertPosition.js
@@ -0,0 +1,34 @@
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+*/
+
+
+const serachInsertPosition = (arr, target) => {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) return mid;
+        if (arr[mid] < target) left = mid + 1;
+        if (arr[mid] > target) right = mid - 1
+    }
+    return left;
+}
+
+
+const value = serachInsertPosition([1, 3, 5, 6], 7);
+
+console.log(value);
