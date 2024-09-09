013b9fa229e3dccef37ecfb571808101dab7fb1b
Sumedh14 <112951142+Sumedh14@users.noreply.github.com>
2024-09-09 10:09:01 +0530

    merge Sort and merging function

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -12,7 +12,7 @@ const mergeSort = (arr1, arr2) => {
     let k = 0;
     const newArray = new Array(m + n).fill(0);
     while (i < m && j < n) {
-        if (arr1[i] < arr2[j]) {
+        if (arr1[i] <= arr2[j]) {
             newArray[k++] = arr1[i++];
         } else {
             newArray[k++] = arr2[j++];
@@ -33,3 +33,49 @@ const arr2 = [1, 4, 8, 10];
 const value = mergeSort(arr1, arr2);
 
 console.log(value);
+
+
+/*
+Merge function of Merge Sort;
+
+input= [10,15,20,11,30]
+
+low = 0;
+mid = 2;
+high = 4;
+
+*/
+
+
+const mergeFunction = (arr, low, mid, high) => {
+    let newArray = new Array(arr.length).fill(0);
+    let j = mid + 1;
+    let i = 0;
+    let k = 0;
+
+    while (i < mid && j < high) {
+        if (arr[low] <= arr[j]) {
+            newArray[k++] = arr[low++];
+        } else {
+            newArray[k++] = arr[j++];
+        }
+    }
+
+    while (low <= mid) {
+        newArray[k++] = arr[low++];
+    }
+    while (j <= high) {
+        newArray[k++] = arr[j++];
+    }
+    return newArray;
+}
+
+
+const input = [10, 15, 20, 11, 30]
+const low = 0;
+const mid = 2;
+const high = 4;
+
+const valuef = mergeFunction(input, low, mid, high);
+
+console.log(valuef);
