af297968abb33ea74ca2837df0214161682dfdc8
Sumedh14 <sumedhbhowate2018@gmail.com>
2025-04-02 09:25:18 +0530

    combination sum

--- tree/backTracking.js
+++ tree/backTracking.js
@@ -38,11 +38,34 @@ function subsetsI(nums) {
 }
 
 
+//combination sum 
 
+function combinationSum(nums, target) {
+    let result = [];
+    let sub = [];
+    const dfs = (nums, target, result, sub, count) => {
+        if (target == 0) {
+            result.push([...sub]);
+        } else if (target < 0 || count >= nums.length) {
+            return;
+        } else {
+            sub.push(nums[count]);
+            dfs(nums, target - nums[count], result, sub, count);
+            sub.pop();
+            dfs(nums, target, result, sub, count + 1);
+        }
+    }
+
+    dfs(nums, target, result, sub, 0)
+    return result;
+}
 
 
 let nums = [1, 2, 3]
 
+
+let combination = combinationSum(nums, 4);
 let valR = subsetsR(nums);
 let valI = subsetsI(nums);
 console.log(valR);
+console.log(combination);
